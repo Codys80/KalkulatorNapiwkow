4dfaadc2c552b8f88c6a63869c8a9fab1c891e33
Codys80 <131345060+Codys80@users.noreply.github.com>
2025-10-31 21:45:08 +0100

    Update 1
    # Dodanie UI
    
    *Dodanie logiki*

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,71 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        int tipPercentage = 15;
+        double currencyMultiplier = 1;
+        double bill = 0;
         public MainPage()
         {
             InitializeComponent();
+            LblSliderValueDisplay.Text = "a";
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        async private void onValueChangeSlider(object sender, EventArgs e)
         {
-            count++;
+            tipPercentage = Convert.ToUInt16(((Slider)sender).Value);
+            LblSliderValueDisplay.Text = tipPercentage.ToString("0.00");
+            CalculateTip();
+        }
+        private void CalculateTip(object sender, EventArgs e)
+        {
+            if (inpValue.Text == null || !inpValue.Text.Trim().All(char.IsDigit) || inpValue.Text == "")
+            {
+                LblTipAmount.Text = "Wpisz poprawną wartość rachunku!";
+                bill = 0;
+                LblTotalAmount.Text = "";
+                return;
+            }
+            CalculateTip();
+        }
+        private void CalculateTip()
+        {
+            if(inpValue.Text == null || !inpValue.Text.Trim().All(char.IsDigit) || inpValue.Text == "")
+            {
+                LblTipAmount.Text = "Wpisz poprawną wartość rachunku!";
+                LblTotalAmount.Text = "";
+                return;
+            }
+            bill = Convert.ToInt16(inpValue.Text.Trim());
+            LblTipAmount.Text = "Napiwek: " + ( (bill * tipPercentage / 100)  * currencyMultiplier).ToString("0.00");
+            LblTotalAmount.Text = "Do zapłaty: " + ( ( bill + (bill * tipPercentage / 100)) * currencyMultiplier ).ToString("0.00");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void ClearFields(object sender, EventArgs e)
+        {
+            tipPercentage = 15;
+            currencyMultiplier = 1;
+            bill = 0;
+            rbPLN.IsChecked = true;
+            inpValue.Text = "";
+            sliderTip.Value = 15;
+            LblTipAmount.Text = "Wyczyszczono!";
+        }
+        private void rbHandler(object sender, EventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "PLN":
+                    currencyMultiplier = 1;
+                    CalculateTip();
+                    break;
+                case "EUR":
+                    currencyMultiplier = 4.25;
+                    CalculateTip();
+                    break;
+                case "USD":
+                    currencyMultiplier = 3.60;
+                    CalculateTip();
+                    break;
+            }
         }
     }
-
 }
